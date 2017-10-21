3ca5946396864a3bc01d7d139f03f6d78a1fc517
Jose Pablo Monge Fierro <josepablomonge96@gmail.com>
2017-10-21 00:20:46 -0700

    first commit

---
+++ app/Http/Controllers/pagesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class pagesController extends Controller
+{
+    public function home(){
+        $messages = [
+            [
+                'id' => 1,
+                'content' => 'Este es mi primer mensaje',
+                'image' => 'https://lorempixel.com/600/338?1',
+            ],
+            [
+                'id' => 2,
+                'content' => 'Este es mi segundo mensaje',
+                'image' => 'https://lorempixel.com/600/338?2',
+            ],
+            [
+                'id' => 3,
+                'content' => 'Este es mi tercer mensaje',
+                'image' => 'https://lorempixel.com/600/338?3',
+            ],
+        ];
+        return view('welcome', ['messages' => $messages]);
+    }
+}
