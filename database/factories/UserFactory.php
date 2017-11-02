0b2ff0e248f52fb00bcc7752fae843a972d1bc95
Jose Pablo Monge Fierro <josepablomonge96@gmail.com>
2017-11-02 15:47:40 -0700

    messages factory and seeding

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,3 +23,10 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Message::class, function (Faker $faker) {
+    return [
+        'content' => $faker->realText(random_int(20,160)),
+        'image' => $faker->imageUrl(600, 338)
+    ];
+});
